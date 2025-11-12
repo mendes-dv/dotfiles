60313bcfb8aa563c7cdb60f3b0f5c4b87c74b8f5
mendes <goncalo.mendes.dev@gmail.com>
2025-11-12 17:47:08 +0000

    make lsp work

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,141 +1,111 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		{ "williamboman/mason.nvim" },
-		{ "williamboman/mason-lspconfig.nvim" },
-		{ "folke/neodev.nvim", opts = {} },
-	},
-	config = function()
-		local nvim_lsp = require("lspconfig")
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		require("neodev").setup({}) -- sets up lua_ls better
-		vim.api.nvim_create_autocmd("LspAttach", {
-			desc = "LSP actions",
-			callback = function(event)
-				local opts = { buffer = event.buf }
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "folke/neodev.nvim", opts = {} },
+  },
+  config = function()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    require("neodev").setup({}) -- Better setup for lua_ls
 
-				vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-				vim.keymap.set("n", "go", "<cmd>Telescope lsp_type_definitions<cr>", opts)
-				vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-				vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-				vim.keymap.set(
-					"n",
-					"<leader>vd",
-					"<cmd>lua vim.diagnostic.open_float()<cr>",
-					{ desc = "View Diagnostics" }
-				)
-				-- Diagnostics
-				vim.keymap.set(
-					"n",
-					"<leader>ed",
-					"<cmd>lua vim.diagnostic.open_float()<cr>",
-					{ buffer = event.buf, desc = "View Diagnostics" }
-				)
-				vim.keymap.set(
-					"n",
-					"ne",
-					"<cmd>lua vim.diagnostic.goto_next()<cr>",
-					{ buffer = event.buf, desc = "Next Diagnostic" }
-				)
-				vim.keymap.set(
-					"n",
-					"np",
-					"<cmd>lua vim.diagnostic.goto_prev()<cr>",
-					{ buffer = event.buf, desc = "Previous Diagnostic" }
-				)
+    -- Attach keymaps
+    vim.api.nvim_create_autocmd("LspAttach", {
+      desc = "LSP actions",
+      callback = function(event)
+        local opts = { buffer = event.buf }
 
-				-- Actions
-				vim.keymap.set("n", "<leader>re", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-				vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-				vim.keymap.set("n", "<leader>.", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-			end,
-		})
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "go", "<cmd>Telescope lsp_type_definitions<cr>", opts)
+        vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, { desc = "View Diagnostics" })
+        vim.keymap.set("n", "<leader>ed", vim.diagnostic.open_float, { buffer = event.buf, desc = "View Diagnostics" })
+        vim.keymap.set("n", "<leader>ne", vim.diagnostic.goto_next, { buffer = event.buf, desc = "Next Diagnostic" })
+        vim.keymap.set("n", "<leader>np", vim.diagnostic.goto_prev, { buffer = event.buf, desc = "Previous Diagnostic" })
+        vim.keymap.set("n", "<leader>re", vim.lsp.buf.rename, opts)
+        vim.keymap.set({ "n", "x" }, "<F3>", function() vim.lsp.buf.format({ async = true }) end, opts)
+        vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, opts)
+      end,
+    })
 
-		vim.lsp.config("basedpyright", {
-			settings = {
-				basedpyright = {
-					disableOrganizeImports = true,
-					analysis = {
-						typeCheckingMode = "basic",
-						diagnosticMode = "workspace",
-						autoSearchPath = true,
-					},
-				},
-			},
-		})
-		vim.lsp.config("ruff", {
-			init_options = {
-				settings = {
-					configurationPreference = "filesystemFirst",
-					fixAll = true,
-					organizeImports = true,
-					lint = {
-						enable = true,
-						preview = true,
-					},
-					format = {
-						preview = true,
-					},
-				},
-			},
-		})
+    -- Modern LSP config using vim.lsp.config
+    vim.lsp.config("pylsp", {
+      capabilities = capabilities,
+      settings = {
+        pylsp = {
+          plugins = {
+            pycodestyle = { enabled = false },
+            pyflakes = { enabled = false },
+            mccabe = { enabled = false },
+            pylint = { enabled = false },
+            yapf = { enabled = false },
+            black = { enabled = false },
+            autopep8 = { enabled = false },
+            ruff = { enabled = true }, -- use ruff-lsp instead
+            pylsp_mypy = { enabled = true, live_mode = false },
+          },
+        },
+      },
+    })
 
-		vim.lsp.config("vtsls", {
-			root_dir = nvim_lsp.util.root_pattern(
-				".git",
-				"pnpm-workspace.yaml",
-				"pnpm-lock.yaml",
-				"yarn.lock",
-				"package-lock.json",
-				"bun.lockb"
-			),
-			typescript = {
-				tsserver = {
-					maxTsServerMemory = 12288,
-				},
-			},
-			experimental = {
-				completion = {
-					entriesLimit = 3,
-				},
-			},
-		})
+    vim.lsp.config("vtsls", {
+      capabilities = capabilities,
+      root_dir = require("lspconfig.util").root_pattern(
+        ".git",
+        "pnpm-workspace.yaml",
+        "pnpm-lock.yaml",
+        "yarn.lock",
+        "package-lock.json",
+        "bun.lockb"
+      ),
+      typescript = {
+        tsserver = {
+          maxTsServerMemory = 12288,
+        },
+      },
+      experimental = {
+        completion = {
+          entriesLimit = 3,
+        },
+      },
+    })
 
-		-- Ensure LSP servers are installed
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"ts_ls",
-				"eslint",
-				"vtsls",
-				"jsonls",
-				"html",
-				"cssls",
-				"gopls",
-				"basedpyright",
-			},
-			automatic_installation = true,
-			automatic_enable = true,
-		})
+    -- Setup Mason to ensure LSPs are installed
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "ts_ls", -- NOTE: not `ts_ls`, check spelling
+        "eslint",
+        "vtsls",
+        "jsonls",
+        "html",
+        "cssls",
+        "gopls",
+        "pylsp",
+      },
+      automatic_installation = true,
+      automatic_enable = true,
+    })
 
-		-- Setup handlers manually
-		local servers = {
-			"lua_ls",
-			"ts_ls",
-			"eslint",
-			"jsonls",
-			"html",
-			"cssls",
-			"gopls",
-			"roslyn",
-		}
+    -- Setup standard LSPs
+    local basic_servers = {
+      "lua_ls",
+      "tsserver",
+      "eslint",
+      "jsonls",
+      "html",
+      "cssls",
+      "gopls",
+    }
 
-		for _, lsp in ipairs(servers) do
-			nvim_lsp[lsp].setup({
-				capabilities = capabilities,
-			})
-		end
-	end,
+    for _, lsp in ipairs(basic_servers) do
+      vim.lsp.config(lsp, {
+        capabilities = capabilities,
+      })
+    end
+  end,
 }
+
+
